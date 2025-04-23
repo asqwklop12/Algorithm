fb4aa83b2d08027569aae642fcc844d50c9a9e04
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2025-04-23 10:27:35 +0900

    [Silver III] Title: 1로 만들기, Time: 124 ms, Memory: 18188 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,23 +1,31 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] d = new int[n + 1];
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int[] maps = new int[n + 10];
 
-        d[1] = 0;  // 1은 연산이 필요 없음
-        for (int i = 2; i <= n; i++) {
-            d[i] = d[i - 1] + 1;  // 기본 연산 (1 빼기)
-            
-            if (i % 2 == 0) {
-                d[i] = Math.min(d[i], d[i / 2] + 1);
-            }
-            if (i % 3 == 0) {
-                d[i] = Math.min(d[i], d[i / 3] + 1);
-            }
-        }
+    maps[1] = 0;
 
-        System.out.println(d[n]);
+    for (int i = 2; i <= n; i++) {
+      maps[i] = maps[i - 1] + 1;
+
+      if (i % 2 == 0) {
+        maps[i] = Math.min(maps[i], maps[i / 2] + 1);
+      }
+
+      if (i % 3 == 0) {
+        maps[i] = Math.min(maps[i], maps[i / 3] + 1);
+      }
     }
+
+    System.out.println(maps[n]);
+
+
+  }
 }
+
+
