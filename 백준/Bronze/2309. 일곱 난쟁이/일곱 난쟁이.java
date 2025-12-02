b0e334cc829cb76ddf1b397960233d05875fec44
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2025-12-02 12:08:48 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 144 ms, Memory: 14808 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int[] dwarfs = new int[9];
+    int sum = 0;
+    for (int i = 0; i < 9; i++) {
+      dwarfs[i] = Integer.parseInt(br.readLine());
+      sum += dwarfs[i];
+    }
+    int[] not = new int[2];
+    for (int i = 0; i < 9; i++) {
+      for (int j = i + 1; j < 9; j++) {
+        if (sum - (dwarfs[i] + dwarfs[j]) == 100) {
+          not[0] = dwarfs[i];
+          not[1] = dwarfs[j];
+        }
+      }
+    }
+    Arrays.sort(dwarfs);
+    for (int i = 0; i < 9; i++) {
+      if(dwarfs[i] == not[0] || dwarfs[i] == not[1]) {
+        continue;
+      }
+      bw.write(dwarfs[i]+"\n");
+    }
+    bw.flush();
+    bw.close();
+  }
+}
