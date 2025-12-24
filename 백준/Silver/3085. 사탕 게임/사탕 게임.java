344f45c36f076db5616e2f7f2793ce6876404307
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2025-12-24 12:08:02 +0900

    [Silver II] Title: 사탕 게임, Time: 272 ms, Memory: 17860 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.java
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.java
@@ -1,64 +1,86 @@
-
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-  static int check(char[][] a) {
-    int n = a.length;
-    int ans = 1;
-    for (int i = 0; i < n; i++) {
-      int cnt = 1;
-      for (int j = 1; j < n; j++) {
-        if (a[i][j] == a[i][j - 1]) {
-          cnt += 1;
-        } else {
-          cnt = 1;
-        }
-        ans = Math.max(ans,cnt);
-      }
-      cnt = 1;
-      for (int j = 1; j < n; j++) {
-        if (a[j][i] == a[j - 1][i]) {
-          cnt += 1;
-        } else {
-          cnt = 1;
-        }
-        ans = Math.max(ans,cnt);
-        
-      }
-    }
-    return ans;
-  }
 
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    char[][] a = new char[n][n];
-    for (int i = 0; i < n; i++) {
-      a[i] = sc.next().toCharArray();
-    }
-    int ans = 0;
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-        if (j + 1 < n) {
-          char t = a[i][j];
-          a[i][j] = a[i][j + 1];
-          a[i][j + 1] = t;
-          ans = Math.max(ans,check(a));
-          t = a[i][j];
-          a[i][j] = a[i][j + 1];
-          a[i][j + 1] = t;
+    static int n;
+    static char[][] arr;
+
+    static int[] dx = { 0, 1, 0, -1 };
+    static int[] dy = { 1, 0, -1, 0 };
+
+    static int max = 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        arr = new char[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String str = sc.next();
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = str.charAt(j);
+            }
         }
-        if (i + 1 < n) {
-          char t = a[i][j];
-          a[i][j] = a[i + 1][j];
-          a[i + 1][j] = t;
-          ans = Math.max(ans,check(a));
-          t = a[i][j];
-          a[i][j] = a[i + 1][j];
-          a[i + 1][j] = t;
-        }
-      }
+
+        solve();
+
     }
-    System.out.println(ans);
-  }
+
+    public static void solve() {
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < 4; k++) {
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+
+                    if (nx < 0 || nx >= n || ny < 0 || ny >= n)
+                        continue;
+                    char t = arr[i][j];
+                    arr[i][j] = arr[nx][ny];
+                    arr[nx][ny] = t;
+                    watch();
+                    // 원상복구
+                    arr[nx][ny] = arr[i][j];
+                    arr[i][j] = t;
+
+                }
+
+            }
+
+        }
+        System.out.println(max);
+    }
+
+    // 일직선
+
+    public static void watch() {
+        for (int i = 0; i < n; i++) {
+            int count = 1;
+            for (int j = 0; j < n - 1; j++) {
+                if (arr[i][j] == arr[i][j + 1]) {
+                    count++;
+                } else {
+                    count = 1;
+                }
+                max = Math.max(max, count);
+
+            }
+            
+        }
+
+        for (int i = 0; i < n; i++) {
+            int count = 1;
+            for (int j = 0; j < n - 1; j++) {
+                if (arr[j][i] == arr[j + 1][i]) {
+                    count++;
+                } else {
+                    count = 1;
+                }
+                 max = Math.max(max, count);
+            }
+           
+        }
+    }
+
 }
