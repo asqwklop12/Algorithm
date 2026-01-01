4a5d5ecbe522d7d96387a031d9c8c90a0d76b37d
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2026-01-01 14:57:29 +0900

    [Silver I] Title: 가장 가까운 세 사람의 심리적 거리, Time: 236 ms, Memory: 24548 KB -BaekjoonHub

--- 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.java
+++ 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.java
@@ -3,15 +3,6 @@ import java.util.*;
 
 public class Main {
 
-    // 입력에 등장한 MBTI만 저장
-    static Map<String, Integer> indexMap;
-    static String[] usedMbti;
-    static int[] count;
-    static int idx;
-
-    static int min;
-    static List<Integer> pick;
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -28,78 +19,34 @@ public class Main {
                 friends[i] = st.nextToken();
             }
 
-            sb.append(solve(friends)).append('\n');
+            solve(N, friends);
         }
 
-        System.out.print(sb.toString());
     }
 
-    public static int solve(String[] friends) {
+    public static void solve(int n, String[] friends) {
+        int result = Integer.MAX_VALUE;
 
-        // ===== 초기화 =====
-        indexMap = new HashMap<>();
-        usedMbti = new String[16];
-        count = new int[16];
-        pick = new ArrayList<>();
-        idx = 0;
-        min = Integer.MAX_VALUE;
-        // ==================
-
-        // MBTI 상태 압축
-        for (String mbti : friends) {
-            if (!indexMap.containsKey(mbti)) {
-                indexMap.put(mbti, idx);
-                usedMbti[idx] = mbti;
-                idx++;
-            }
-            count[indexMap.get(mbti)]++;
-        }
-
-        // 중복 조합 DFS
-        dfs(0, 0);
-
-        return min;
-    }
-
-    static void dfs(int start, int depth) {
-
-        // 가지치기: 최소는 0
-        if (min == 0)
-            return;
-
-        if (depth == 3) {
-            int a = pick.get(0);
-            int b = pick.get(1);
-            int c = pick.get(2);
-
-            // 필요한 인원 수 체크
-            int[] need = new int[16];
-            need[a]++;
-            need[b]++;
-            need[c]++;
-
-            for (int i = 0; i < idx; i++) {
-                if (need[i] > count[i])
-                    return; // 불가능한 조합
-            }
-
-            String s1 = usedMbti[a];
-            String s2 = usedMbti[b];
-            String s3 = usedMbti[c];
-
-            int dist = distance(s1, s2) +
-                    distance(s1, s3) +
-                    distance(s2, s3);
-
-            min = Math.min(min, dist);
+        if (n > 32) {
+            System.out.println(0);
             return;
         }
 
-        for (int i = start; i < idx; i++) {
-            pick.add(i);
-            dfs(i, depth + 1);
-            pick.remove(pick.size() - 1);
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    int dist = distance(friends[i], friends[j]) +
+                            distance(friends[i], friends[k]) +
+                            distance(friends[j], friends[k]);
+
+                    result = Math.min(result, dist);
+                }
+            }
+
         }
+
+        System.out.println(result);
+
     }
 
     static int distance(String a, String b) {
